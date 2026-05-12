12235367459a9d8db57bc04812fa6437ce57be3b
chubokmvx-dev <chubokmvx@gmail.com>
2026-05-12 22:22:19 +0300

    Update config.py

--- config.py
+++ config.py
@@ -10,21 +10,23 @@ UKRAINE_FIRST_LEAGUE_ID = 334
 
 RSS_FEEDS = [
     "https://www.0372.ua/rss/news",
+    "https://chernivtsi.online/feed/",
+    "https://molbuk.ua/rss.xml",
+    "https://cheline.com.ua/rss",
+    "https://suspilne.media/chernivtsi/rss/",
     "https://suspilne.media/rss/",
     "https://ukrinform.ua/rss/block-society",
     "https://www.epravda.com.ua/rss/",
     "https://tsn.ua/rss/full.rss",
-    "https://www.rbc.ua/ukr/rss.xml",
     "https://mind.ua/rss",
     "https://www.ukrainealarm.com/rss",
-    "https://suspilne.media/chernivtsi/rss/",
     "https://t.me/s/ppo_alert",
-
 ]
+
 
 NEWS_FETCH_INTERVAL_MIN     = 30
 FOOTBALL_CHECK_INTERVAL_MIN = 3
 MORNING_DIGEST_TIME         = "08:00"
 EVENING_DIGEST_TIME         = "20:00"
-MAX_POSTS_PER_CYCLE = 1
+MAX_POSTS_PER_CYCLE = 2
 MAX_POLLS_PER_DAY   = 2
